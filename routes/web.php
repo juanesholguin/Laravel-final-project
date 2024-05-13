e593ad5850457bfde729825dd48f8e90607ed2b5
jeholguin <jeholguin@hiberus.com>
2024-05-13 15:00:52 -0500

    fix: remove native register url

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Auth::routes();
+Auth::routes(['register' => false]);
 Route::redirect('/home', '/');
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
