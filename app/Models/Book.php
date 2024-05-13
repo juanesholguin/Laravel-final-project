3b625bcfaf790e7be4ed8b6a51e4579ee35c5f6a
jeholguin <jeholguin@hiberus.com>
2024-05-13 10:48:16 -0500

    feat: Add slug config

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,15 +4,35 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Book extends Model
 {
     use HasFactory;
+    use Sluggable;
 
     protected $fillable = ['title', 'author', 'price', 'cover_image', 'published_at', 'category_id'];
 
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * @return array
+     */
+    public function sluggable(): array {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    public function getRouteKeyName(): string {
+        return 'slug';
     }
 }
